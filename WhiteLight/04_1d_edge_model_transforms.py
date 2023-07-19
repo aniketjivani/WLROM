11a6c3aa5234804feb1a7d296e8ad38f97da0564
Aniket J Maverick <ajivani@umich.edu>
2023-07-18 22:10:54 -0400

    start afresh for edges and get correct polar coordinate representations of edges

--- WhiteLight/04_1d_edge_model_transforms.py
+++ WhiteLight/04_1d_edge_model_transforms.py
@@ -13,9 +13,6 @@
 #     name: python3
 # ---
 
-# %% [markdown]
-# Load stacked edge data, and write basic viz functions to plot edges etc.
-
 # %%
 import numpy as np
 import pandas as pd
@@ -78,38 +75,15 @@ nSims_2154 = ed_2154.shape[2]
 nSims_2161, nSims_2192, nSims_2154
 
 # %% [markdown]
-# Workflow to filter data before it reaches boundary i.e. look for values going along indices above 120 , pick the minimum row index as cutoff for entire range of angles, and set that as the time up to which we have training and or testing data.
-
-# %%
-R_inner = 4.05
-R_outer = 23.6
-
-# %%
-height_flattened=128
-np.arange(0, height_flattened, step=21)
-
-# %%
-hts_pixel = np.linspace(0, height_flattened, height_flattened + 1)
-hts_pixel
-
-# %%
-# is this correct?
-radii = ((hts_pixel)/height_flattened)*(R_outer - R_inner) + R_inner
-radii
-
-# %%
-ed_2161_scaled = (ed_2161/height_flattened)*(R_outer - R_inner) + R_inner
-ed_2192_scaled = (ed_2192/height_flattened)*(R_outer - R_inner) + R_inner
-ed_2154_scaled = (ed_2192/height_flattened)*(R_outer - R_inner) + R_inner
-
-# %% [markdown]
-# Adapted function from `plot_heat_data` in previous notebooks.
-
-# %% [markdown]
-# In plotting we need to convert axes indices to actual theta and R values (also control the ticks) - follow basic outline of `Kmeans_CR2154_SignedDist` from Hongfan.
-
-# %% [markdown]
-# **Calculation for angles**
+# **Event: Start, End(x out of 512)**
+#
+# CR2154: 50, 450
+#
+# CR2161: 160,360
+#
+# CR2192: 90, 275
+#
+# So the actual angle(suppose (0,1)i s 0 and (1,0) is 90 and so forth) will be 360/512 * x + 1.2 * 180 - 360. Note that, 1.2pi is coming from function Normalization, in which I start at x+1.2pi counterclockwise.Here's an example, for CR2161, the actual angle will be seq(0,360, by = 360/512)[160:359]+1.2*180 - 360 which is equal to [-32, 107]
 
 # %%
 theta_s_2161, theta_e_2161 = np.linspace(0, 360, 512)[160] + 1.2 * 180 - 360, np.linspace(0, 360, 512)[359] + 1.2 * 180 - 360
@@ -121,34 +95,11 @@ print("Range of angles for CR2161: {} {}".format(theta_s_2192, theta_e_2192))
 theta_s_2154, theta_e_2154 = np.linspace(0, 360, 512)[50] + 1.2 * 180 - 360, np.linspace(0, 360, 512)[449] + 1.2 * 180 - 360
 print("Range of angles for CR2161: {} {}".format(theta_s_2154, theta_e_2154))
 
-# %% [markdown]
-# ~~Can we also make a Cartesian plot? easier to see all edges progressing outward (with overlaid radius of occulting disk) and get a better sense of the shape.~~
-#
-# Save for all sims and events.
+# %%
+height_flattened=128
 
 # %%
 actualTimes = np.arange(2, 182, 2)
-
-# %%
-edge_save_dir = "./edge_img"
-
-# %%
-ed_2192.max()
-
-# %% [markdown]
-# **Threshold the edges**
-#
-# Set a cutoff time for plotting the edges, because if edge reaches the boundary very quickly, the algorithm breaks down after the cutoff and can result in incorrect edges (0 values, non-monotonic behaviour etc). so we need to filter the data correctly before using it for plotting / training purposes.
-
-# %%
-# cutoff_value = 120
-# t_cutoff_idx = []
-# for j in range(ed_2161.shape[2]):
-#     for i in range(ed_2161.shape[0]):
-#         boundary_idx = np.where(ed_2161[i, :, j])
-
-# %%
-plt.plot([1, 2, 4], [8, 9, 10])
 
 
 # %%
@@ -178,6 +129,7 @@ def plot_edge_data(Z, titleSim, eventID=2161, plotAll=False, ax=None, theta=np.l
     ax.legend(loc=(1.05, .05))
     ax.set_title("Edge data Sim {:03d}".format(titleSim))
 
+
     # %%
     # commenting out save functionality for now!!
     #     plt.savefig(os.path.join(edge_save_dir, "CR{}".format(eventID), "run_{:03d}.png".format(titleSim)),
@@ -187,32 +139,8 @@ def plot_edge_data(Z, titleSim, eventID=2161, plotAll=False, ax=None, theta=np.l
 
     #     plt.close()
 
-
-# %%
-def save_event_edge_data(eventID, plotAll=False, ax=None, theta_start=160, theta_end=360):
-    # call data simwise, get sim labels
-    edge_data, sim_data = load_edge_data(eventID)
-    edge_data = (edge_data/height_flattened)*(R_outer - R_inner) + R_inner
-    # make call to plotting function (also saves image) - MultiProcessing?
-    nSims = edge_data.shape[2]
-    
-    theta_range = np.linspace(0, 360, 512)[theta_start:theta_end] + 1.2 * 180 - 360
-    for i in range(nSims):
-        plot_edge_data(edge_data[:, :, i].T, sim_data[i], eventID=eventID, 
-                       plotAll=False, theta=theta_range)        
-
-
-# %%
-ed_2161.shape
-
 # %%
 plot_edge_data(ed_2161[:, :, 0].T, 31, eventID=2161, plotAll=False, ax=None, theta=np.linspace(-32, 107, 200))
-
-# %% [markdown]
-# Are our ylims getting set correctly in the figure?
-
-# %% [markdown]
-# A problem with the approach is that blindly plotting edges means that we will plot lines even where the algorithm is expected to stop working reliably i.e. where the edge has reached the boundary or is too slow and the lack of monotonicity means a lot of overlap with the edges at previous timesteps.
 
 # %%
 edge1 = ed_2161[20, :, 0]
@@ -222,15 +150,11 @@ plt.plot(edge1, label="Edge t1")
 plt.plot(edge2, label="Edge t2")
 plt.legend()
 
-# %% [markdown]
-# For one sim, e.g. sim 31, find the min time at which min edge value is greater than zero. That has to be our starting point, else part of it is unobserved and we can't really use that for training.
-
 # %%
 ed31 = ed_2161[:, :, 0]
 
 # %%
 min_edge_by_time = np.min(ed31, axis=1)
-min_edge_by_time
 
 # %%
 tMinIdx = np.where(min_edge_by_time > 0)[0][0]
@@ -251,9 +175,6 @@ theta_all = np.linspace(-32, 107, 200)
 r_by_t_ref = ed31[tMinIdx, 0:-1:2]/tMin
 
 # %%
-r_by_t_ref
-
-# %%
 t2 = tMin + 2
 t2
 
@@ -262,108 +183,6 @@ r2_exact = r_by_t_ref * t2
 
 # %%
 r2_all = ed31[tMinIdx + 1, :]
-
-# %%
-plt.scatter(theta_samples, r2_exact, c=theta_samples, cmap='Greens')
-
-# %% [markdown]
-# Now set up interpolation to figure out the closest $\theta$ values where we can match to the calculated `r2`.
-
-# %%
-order = r2_all.argsort()
-y_data = r2_all[order]
-x_data = theta_all[order]
-
-# %% [markdown]
-# I think the above interpolation is going wrong. Theta should follow the parabolic curve but because of non-unique $r$ the sorting is not putting the $\theta$ in correct order. Maybe we should try piecewise sorting instead!!
-
-# %%
-plt.plot(np.diff(r2_all))
-
-# %% [markdown]
-# We really need to calculate radius instead of pixel values. Its probably not a linear map, that's another reason why we may be going wrong!!!!
-
-# %%
-theta_interp = np.interp(r2_exact, y_data, x_data)
-theta_interp
-
-# %% [markdown]
-# Plot all interpolated $\theta$ and $r$ values.
-
-# %%
-plt.scatter(theta_interp, r2_exact, label="Interpolated")
-plt.plot(theta_all, edge1, label="Edge t1")
-plt.plot(theta_all, edge2, label="Edge t2")
-plt.legend()
-
-# %% [markdown]
-# Plot $r$ on x-axis and $\theta$ on y-axis. Try `scipy.interpolate` method and see if the result is any different. Do we have to change the scaling to make this work?
-
-# %%
-plt.plot(r2_all, theta_all, label="theta = f(r)")
-plt.xlabel("r")
-plt.ylabel("θ")
-plt.legend()
-
-# %%
-spl = CubicSpline(y_data, x_data)
-plt.plot(r2_all, theta_all, label="theta = f(r)")
-plt.plot(r2_all[0:-1:2], spl(r2_all[0:-1:2]), 'o', label="Interpolated for curve")
-plt.plot(r2_exact, spl(r2_exact), 'o', label="Interpolated for calculated r")
-
-
-plt.xlabel("r")
-plt.ylabel("θ")
-plt.legend()
-
-# %% [markdown]
-# **Approach 1**: Can we change sampling procedure too (non-uniform)? First, we need to deal with the non-monotonicity correctly. Some help thankfully from https://stackoverflow.com/questions/48028766/get-x-values-corresponding-to-y-value-on-non-monotonic-curves
-
-# %%
-import numpy as np
-from scipy.interpolate import interpn
-
-
-# y_data and x_data are sorted data.
-
-theta_direction = np.sign(np.diff(x_data))
-
-# %%
-# import numpy as np
-# from scipy.interpolate import interp1d
-
-# # convert data lists to arrays
-# x, y = np.array(x), np.array(y)
-
-# # sort x and y by x value
-# order_vals = np.argsort(x)
-# xsort, ysort = x[order_vals], y[order_vals]
-
-# # compute indices of points where y changes direction
-# ydirection = np.sign(np.diff(ysort))
-# changepoints = 1 + np.where(np.diff(ydirection) != 0)[0]
-
-# # find groups of x and y within which y is monotonic
-# xgroups = np.split(xsort, changepoints)
-# ygroups = np.split(ysort, changepoints)
-# interps = [interp1d(y, x, bounds_error=False) for y, x in zip(ygroups, xgroups)]
-
-# # interpolate all y values
-# yval = 100
-# xvals = np.array([interp(yval) for interp in interps])
-
-# print(xvals)
-
-# %%
-
-# %%
-
-# %%
-
-# %%
-
-# %% [markdown]
-# **Approach 2**: Nearest neighbours? Find closest matching $(r/t)$ values in edge at next timestep to the previous, and simply use those so that interpolation doesn't have to be optimized (interpolation goes the loong and possibly too complicated? way round trying to find the right $\theta$).
 
 # %%
 r_by_t_calc = ed31[tMinIdx + 1, 0:-1:2]/(tMin + 2)
@@ -387,20 +206,18 @@ plt.legend()
 
 
 # %%
+# convert pixel values to Cartesian, then get correct R and theta values from there. Show on a polar plot!
 
 # %% [markdown]
-# Check how to convert pixel values into radius correctly. 
+# **Event: Start, End(x out of 512)**
 #
-# 1. Paste the edge to cartesian conversion function here. Check if the results match up to Y labels we have been using in multiple edge plots
-
-# %% [markdown]
-# For angles (from Hongfan):
-# ```
-# Event: Start, End(x out of 512)
 # CR2154: 50, 450
+#
 # CR2161: 160,360
-# CR2192: 90, 275So the actual angle(suppose (0,1)i s 0 and (1,0) is 90 and so forth) will be 360/512 * x + 1.2 * 180 - 360. Note that, 1.2pi is coming from function Normalization, in which I start at x+1.2pi counterclockwise.Here's an example, for CR2161, the actual angle will be seq(0,360, by = 360/512)[160:359]+1.2*180 - 360 which is equal to [-32, 107]
-# ```
+#
+# CR2192: 90, 275
+#
+# So the actual angle(suppose (0,1)i s 0 and (1,0) is 90 and so forth) will be 360/512 * x + 1.2 * 180 - 360. Note that, 1.2pi is coming from function Normalization, in which I start at x+1.2pi counterclockwise.Here's an example, for CR2161, the actual angle will be seq(0,360, by = 360/512)[160:359]+1.2*180 - 360 which is equal to [-32, 107]
 
 # %%
 def Polar_to_Cartesian(edge, start_angle, end_angle, height, width, circles_disk, circles_scope):
@@ -416,103 +233,173 @@ def Polar_to_Cartesian(edge, start_angle, end_angle, height, width, circles_disk
     Xi = circle_x + r_scope * np.cos(theta + 1.2 * np.pi)
     Yi = circle_y + r_scope * np.sin(theta + 1.2 * np.pi)
     r = edge / height
-    return r + 4.05
-# Aniket: Lightly modifying the above to return r instead of X and Y
-#     X = Xp + ( Xi - Xp ) * r
-#     Y = Yp + ( Yi - Yp ) * r
-#     return (X,Y)
+    X = Xp + ( Xi - Xp ) * r
+    Y = Yp + ( Yi - Yp ) * r
+    return (X,Y)
 
 
 # %%
-height_flattened
+X1, Y1 = Polar_to_Cartesian(edge1, 
+                   start_angle = 160, 
+                   end_angle = 360, 
+                   height=128, 
+                   width=512, 
+                   circles_disk=(149,149,19), 
+                   circles_scope=(149,149,110))
 
 # %%
-np.arange(0, height_flattened, step=21)
+X2, Y2 = Polar_to_Cartesian(edge2, 
+                   start_angle = 160, 
+                   end_angle = 360, 
+                   height=128, 
+                   width=512, 
+                   circles_disk=(149,149,19), 
+                   circles_scope=(149,149,110))
 
 # %%
-Polar_to_Cartesian(np.arange(0, height_flattened, step=21),
-                   start_angle=160,
-                   end_angle=360,
-                   height=128,
-                   width=512,
-                   circles_disk=(149, 149, 19),
-                   circles_scope=(149, 149, 110))
+X1_norm, Y1_norm = (64 * (X1/300) - 32), (64 * (Y1/300) - 32)
+X2_norm, Y2_norm = (64 * (X2/300) - 32), (64 * (Y2/300) - 32)
+
+# %%
+R1, theta1 = np.sqrt(X1_norm**2 + Y1_norm**2), np.arctan2(Y1_norm, X1_norm)
+R2, theta2 = np.sqrt(X2_norm**2 + Y2_norm**2), np.arctan2(Y2_norm, X2_norm)
+
+# %%
+fig, ax = plt.subplots(subplot_kw={'projection': 'polar'})
+ax.plot(theta1, R1, linewidth=1.5, label="Edge t1")
+ax.plot(theta2, R2, linewidth=1.5, label="Edge t2")
+ax.tick_params(axis='both', which='major', labelsize=6)
+ax.set_rmax(24)
+ax.grid(True)
+# For polar axes, it may be useful to move the legend slightly away from the
+# axes center, to avoid overlap between the legend and the axes.  The following
+# snippet places the legend's lower left corner just outside the polar axes
+# at an angle of 67.5 degrees in polar coordinates.
+angle = np.deg2rad(67.5)
+ax.legend(loc="lower left",
+          prop={'size': 6},
+          bbox_to_anchor=(.5 + np.cos(angle)/2, .5 + np.sin(angle)/2))
 
 # %% [markdown]
-# Nope this is not correct.
+# Also plot the above on a regular Cartesian plot (unlike the previous ones, this one will have R as the y-axis!)
 
 # %%
+plt.plot(theta1 * 180 / np.pi, R1, label="Edge 1")
+plt.plot(theta2 * 180 / np.pi, R2, label="Edge 2")
+plt.xlabel("θ")
+plt.ylabel("R")
+plt.legend()
 
-# %%
-
-# %%
-
-# %%
-
-# %%
-
-# %%
-# # commented out call for saving edge data
-# save_event_edge_data(2161, plotAll=False, ax=None, theta_start=160, theta_end=360)
-# save_event_edge_data(2192, plotAll=False, ax=None, theta_start=90, theta_end=275)
-# save_event_edge_data(2154, plotAll=False, ax=None, theta_start=50, theta_end=450)
-
-# %%
-# POLAR AXES PLOT
-# fig, ax = plt.subplots(subplot_kw={'projection': 'polar'})
-# # r = np.linspace(4.05, 23.6, height_flattened + 1)
-# theta = np.linspace(-32, 107, 90) * np.pi / 180
-# ax.plot(theta, ed_2161_scaled[:, :, 0].T[25, :])
-# ax.plot(theta, ed_2161_scaled[:, :, 0].T[40, :])
-# ax.plot(theta, ed_2161_scaled[:, :, 0].T[50, :])
-
-# %%
-# plot_edge_data(edge_data_cutoff.T, "Edge data Sim 31", plotAll=False)
 
 # %% [markdown]
-# **Scratch Begin**
+# Now rerun the polar conversion for all the edges. Make a side by side plot for visualization first. We can also set the $\theta$ values to a common range as we had already prespecified. For now arguments like start and end angle are event-specific.
 
 # %%
-aaa = np.array([[1, 2, 3],
-               [4, 5, 6],
-               [7, 8, 9]])
+def getRValues(edge_data_matrix, simIdx=0, minStartIdx=20):
+    
+    edges_sim = edge_data_matrix[minStartIdx:, :, simIdx]
+    
+    nTimes, nThetas = edges_sim.shape[0], edges_sim.shape[1]
+    
+    r_vals = np.zeros((nTimes, nThetas))
+    theta_vals = np.zeros((nTimes, nThetas))
+    
+    for i in range(nTimes):
+        xi, yi   = Polar_to_Cartesian(edges_sim[i, :], 
+                   start_angle = 160, 
+                   end_angle = 360, 
+                   height=128, 
+                   width=512, 
+                   circles_disk=(149,149,19), 
+                   circles_scope=(149,149,110))
+        
+        xi_norm, yi_norm = 64 * (xi/300) - 32, 64 * (yi/300) - 32
+        theta_vals[i, :] = np.arctan2(yi_norm, xi_norm)
+    
+        r_vals[i, :] = np.sqrt(xi_norm**2 + yi_norm**2)
+    
+    return r_vals, theta_vals
+
 
 # %%
-np.min(aaa, axis=1)
+r_vals_s31, theta_vals_s31 = getRValues(ed_2161, simIdx=0, minStartIdx = 20)
 
 # %%
-edge_data_31 = ed_2161[:, :, 0]
+fig = plt.figure(figsize=(12, 6))
+ax1 = plt.subplot(121)
+ax2 = plt.subplot(122, projection='polar')
 
-# %%
-tIdx = min(np.unique(np.where(edge_data_31 >= 120)[0]))
-tIdx
+sampleTimeIdx = [0, 2, 4, 8, 12, 20, 40, 50, 60, 66, 69]
+sampleTimesToPlot = actualTimes[20:][sampleTimeIdx]
 
-# %%
-tCutoff = actualTimes[tIdx]
-tCutoff
+color = iter(plt.cm.viridis(np.linspace(0, 1, len(sampleTimesToPlot))))
 
-# %%
-edge_data_cutoff = edge_data_31[:tIdx, :]
-edge_data_cutoff.shape
+theta = np.linspace(-32, 107, 200)
 
-# %%
-sample_columns = [0, 2, 4, 8, 12, 20, 40, 50, 60, 70]
-sample_times = actualTimes[sample_columns]
-sample_times
+for i, j in enumerate(sampleTimeIdx):
+    ax1.plot(theta, r_vals_s31[j, :], color=next(color), label=fr"$u(t_{{{sampleTimesToPlot[i]}}})$")
 
-# %%
-basis = opinf.pre.PODBasis().fit(edge_data_cutoff.T, residual_energy=1e-8)
-print(basis)
+ax1.set_xlabel(r"$θ (deg)$")
+ax1.set_ylabel(r"$r_{edge}(t)$")
+ax1.set_xlim(-32, 107)
+ax1.set_ylim(4.05, 24)
+ax1.legend(loc=(1.05, .05))
 
-# Check the decay of the singular values.
-basis.plot_svdval_decay()
-plt.xlim(0, 25)
+color = iter(plt.cm.viridis(np.linspace(0, 1, len(sampleTimesToPlot))))
 
-# %%
-plt.plot(basis.svdvals/basis.svdvals[1])
-plt.xlim(0, 10)
+
+for i, j in enumerate(sampleTimeIdx):
+    ax2.plot(theta * np.pi/180, r_vals_s31[j, :], color=next(color), label=fr"$u(t_{{{sampleTimesToPlot[i]}}})$")
+
+
+ax2.tick_params(axis='both', which='major', labelsize=10)
+ax2.set_rmax(24)
+ax2.grid(True)
+
+
+fig.tight_layout()
 
 # %% [markdown]
-# **Scratch End**
+# Visualize matching of rescaled points with Dynamic Time Warping and see if we can get a unique correspondence?
+
+# %%
+
+# %%
+
+# %%
+
+# %% [markdown]
+# **Proper Orthogonal Decomposition**?
+
+# %%
+
+# %%
+
+# %%
+
+# %% [markdown]
+# SOpInf style, we could shift and scale first, but then the drawback is that the shift learning process is not explicitly parametrized? Maybe then the shifting problem could just be treated separately, i.e.
+#
+# **Model 1**: Given new $\theta^\ast$, predict $c(\theta^\ast)$ based on training set
+#
+# **Model 2**: Predict $\hat{f}(\theta^\ast, r/t)$ and then shift back to advection coordinates (add $c(\theta^\ast) \times t$) to get $f(\theta^\ast, r, t)$
+
+# %%
+
+# %%
+
+# %%
+
+# %%
+
+# %%
+
+# %% [markdown]
+# **Non linear dimensionality Reduction - Diffusion Maps?**
+
+# %% [markdown]
+# [Coifman and Lafon, 2006 - Diffusion Maps](https://doi.org/10.1016/j.acha.2006.04.006)
+#
+# [Winstead, 2017, Nonlinear MOR based on Manifold Learning](https://trace.tennessee.edu/cgi/viewcontent.cgi?article=5535&context=utk_gradthes)
 
 # %%
